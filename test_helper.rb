403f119e37baf325a7cd16d6a4a63c36353d48bd
Andrei <138538471+thephantomsage@users.noreply.github.com>
2023-07-18 11:13:05 +0800

    Update test_helper.rb

--- test_helper.rb
+++ test_helper.rb
@@ -8,6 +8,6 @@ class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+#this is a comment hahaha
   # Add more helper methods to be used by all tests here...
 end
